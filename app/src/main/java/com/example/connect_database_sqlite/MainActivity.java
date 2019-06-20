67bfafe31159082788da53c687519f9e18777097
unknown <himashms98@yahoo.com>
2019-06-20 18:35:36 +0200

    first commit

---
+++ app/src/main/java/com/example/connect_database_sqlite/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.connect_database_sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void save(View view) {
+
+    }
+}
